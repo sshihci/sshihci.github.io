0c6b94a7ba08b35655abc56f5af6c38d5e2f82c4
YutaUra <yuuta3594@outlook.jp>
2021-11-02 19:23:12 +0900

    eslintの導入

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,12 +4,12 @@ import * as React from 'react'
 // styles
 const pageStyles: React.CSSProperties = {
   color: '#232129',
-  padding: '96px',
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
+  padding: '96px',
 }
 const headingStyles: React.CSSProperties = {
-  marginTop: 0,
   marginBottom: 64,
+  marginTop: 0,
   maxWidth: 320,
 }
 
@@ -17,35 +17,51 @@ const paragraphStyles: React.CSSProperties = {
   marginBottom: 48,
 }
 const codeStyles: React.CSSProperties = {
-  color: '#8A6534',
-  padding: 4,
   backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
   borderRadius: 4,
+  color: '#8A6534',
+  fontSize: '1.25rem',
+  padding: 4,
 }
 
 // markup
-const NotFoundPage = () => {
+const NotFoundPage = (): JSX.Element => {
   return (
     <main style={pageStyles}>
       <title>Not found</title>
+
       <h1 style={headingStyles}>Page not found</h1>
+
       <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
+        <span>Sorry</span>
+
+        <span aria-label="Pensive emoji" role="img">
           😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
+        </span>
+
+        <span>we couldn’t find what you were looking for.</span>
+
         <br />
+
         {process.env['NODE_ENV'] === 'development' ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+
+            <span>Try creating a page in</span>
+
+            <code style={codeStyles}>src/pages/</code>
+
+            <span>.</span>
+
             <br />
           </>
         ) : null}
+
         <br />
-        <Link to="/">Go home</Link>.
+
+        <Link to="/">Go home</Link>
+
+        <span>.</span>
       </p>
     </main>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import * as React from 'react'
 // styles
 const pageStyles: React.CSSProperties = {
   color: '#232129',
-  padding: 96,
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
+  padding: 96,
 }
 const headingStyles: React.CSSProperties = {
-  marginTop: 0,
   marginBottom: 64,
+  marginTop: 0,
   maxWidth: 320,
 }
 const headingAccentStyles: React.CSSProperties = {
@@ -18,27 +18,27 @@ const paragraphStyles: React.CSSProperties = {
   marginBottom: 48,
 }
 const codeStyles: React.CSSProperties = {
-  color: '#8A6534',
-  padding: 4,
   backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
   borderRadius: 4,
+  color: '#8A6534',
+  fontSize: '1.25rem',
+  padding: 4,
 }
 const listStyles: React.CSSProperties = {
   marginBottom: 96,
   paddingLeft: 0,
 }
 const listItemStyles: React.CSSProperties = {
-  fontWeight: 300,
   fontSize: 24,
-  maxWidth: 560,
+  fontWeight: 300,
   marginBottom: 30,
+  maxWidth: 560,
 }
 
 const linkStyle: React.CSSProperties = {
   color: '#8954A8',
-  fontWeight: 'bold',
   fontSize: 16,
+  fontWeight: 'bold',
   verticalAlign: '5%',
 }
 
@@ -51,128 +51,142 @@ const docLinkStyle: React.CSSProperties = {
 const descriptionStyle: React.CSSProperties = {
   color: '#232129',
   fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
   lineHeight: 1.25,
+  marginBottom: 0,
+  marginTop: 10,
 }
 
 const docLink = {
+  color: '#8954A8',
   text: 'Documentation',
   url: 'https://www.gatsbyjs.com/docs/',
-  color: '#8954A8',
 }
 
 const badgeStyle: React.CSSProperties = {
-  color: '#fff',
   backgroundColor: '#088413',
   border: '1px solid #088413',
+  borderRadius: 4,
+  color: '#fff',
+  display: 'inline-block',
   fontSize: 11,
   fontWeight: 'bold',
   letterSpacing: 1,
-  borderRadius: 4,
+  lineHeight: 1,
+  marginLeft: 10,
   padding: '4px 6px',
-  display: 'inline-block',
   position: 'relative',
   top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
 }
 
 // data
 const links = [
   {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
+    color: '#E95800',
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
+    text: 'Tutorial',
+    url: 'https://www.gatsbyjs.com/docs/tutorial/',
   },
   {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
+    color: '#1099A8',
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
+    text: 'How to Guides',
+    url: 'https://www.gatsbyjs.com/docs/how-to/',
   },
   {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
+    color: '#BC027F',
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
+    text: 'Reference Guides',
+    url: 'https://www.gatsbyjs.com/docs/reference/',
   },
   {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
+    color: '#0D96F2',
     description:
       'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
+    text: 'Conceptual Guides',
+    url: 'https://www.gatsbyjs.com/docs/conceptual/',
   },
   {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
+    color: '#8EB814',
     description:
       'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
+    text: 'Plugin Library',
+    url: 'https://www.gatsbyjs.com/plugins',
   },
   {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
     badge: true,
+    color: '#663399',
     description:
       'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399',
+    text: 'Build and Host',
+    url: 'https://www.gatsbyjs.com/cloud',
   },
 ]
 
 // markup
-const IndexPage = () => {
+const IndexPage = (): JSX.Element => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
+
       <h1 style={headingStyles}>
-        Congratulations
+        <span>Congratulations</span>
+
         <br />
+
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
+
+        <span aria-label="Party popper emojis" role="img">
           🎉🎉🎉
         </span>
       </h1>
+
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{' '}
-        <span role="img" aria-label="Sunglasses smiley emoji">
+        <span>Edit</span>
+
+        <code style={codeStyles}>src/pages/index.js</code>
+
+        <span>to see this page update in real-time.</span>
+
+        <span aria-label="Sunglasses smiley emoji" role="img">
           😎
         </span>
       </p>
+
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
-            style={linkStyle}
             href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+            style={linkStyle}
           >
             {docLink.text}
           </a>
         </li>
+
         {links.map((link) => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
               <a
-                style={linkStyle}
                 href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                style={linkStyle}
               >
                 {link.text}
               </a>
+
               {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
+                <span aria-label="New Badge" style={badgeStyle}>
                   NEW!
                 </span>
               )}
+
               <p style={descriptionStyle}>{link.description}</p>
             </span>
           </li>
         ))}
       </ul>
+
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
