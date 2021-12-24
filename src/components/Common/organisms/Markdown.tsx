2439081cbfea920b51b319694bbe825639547488
YutaUra <yuuta3594@outlook.jp>
2021-12-24 20:10:26 +0900

    how to create news

--- src/components/Common/organisms/Markdown.tsx
+++ src/components/Common/organisms/Markdown.tsx
@@ -1,9 +1,11 @@
+import type { Root } from 'hast'
 import { ComponentType, createElement } from 'react'
 import rehypeReact from 'rehype-react'
 import { unified } from 'unified'
 import { File, FileProps } from './File'
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface IntrinsicElements {
       file: ComponentType<typeof File>
@@ -14,16 +16,16 @@ declare global {
 const processor = unified().use(rehypeReact, {
   components: {
     file: ({ name }: FileProps) => {
-      return <File name={name} className="no-underline max-w-max" />
+      return <File className="max-w-max no-underline" name={name} />
     },
   },
   createElement,
 })
 
 type MarkdownProps = {
-  htmlAst: any
+  htmlAst: unknown
 }
 
 export const Markdown = ({ htmlAst }: MarkdownProps): JSX.Element => {
-  return processor.stringify(htmlAst)
+  return processor.stringify(htmlAst as Root)
 }
